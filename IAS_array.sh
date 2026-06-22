29402f4c368b2398d3a3c579ef20915f7d0032c1
EdwardMoseley <etmoseley@yahoo.com>
2026-06-22 16:01:12 -0400

    2LOB update

--- IAS_array.sh
+++ IAS_array.sh
@@ -6,7 +6,7 @@
 ## Call with:
 # sbatch --array=1-4%1 IAS_array.sh Boltz_3EIC_MKI_model_0001.pdb A203 A204 A207 A217
 # B4
-# sbatch --array=1-4%1 IAS_array.sh 2LOB_Model_One.clean.pdb A50 A89 A90 A91 A92 A93 A95
+# sbatch --array=1-11%1 IAS_array.sh 2LOB_Model_One.clean.pdb A40 A41 A42 A43 A44 A45 A91 A95 A98 A105 A107
 
 set -euo pipefail
 
